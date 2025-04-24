7d648f2a0f17e3a6fa95f0d9127a6d74ad1429cc
afkorigin <gnstldgk@naver.com>
2025-04-24 17:20:06 +0900

    first

---
+++ src/main/java/com/portalSite/PortalSiteApplication.java
@@ -0,0 +1,13 @@
+package com.portalSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portalSite/PortalSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portalSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
