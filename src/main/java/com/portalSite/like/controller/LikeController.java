5c1d24dc1dc834f5d82b3494a204ea5849972ede
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-04-26 02:22:18 +0900

    ✨Feature : 좋아요 CRUD 완성

---
+++ src/main/java/com/portalSite/like/controller/LikeController.java
@@ -0,0 +1,40 @@
+package com.portalSite.like.controller;
+
+import com.portalSite.like.entity.Like;
+import com.portalSite.like.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/likes")
+@RequiredArgsConstructor
+public class LikeController {
+    private final LikeService likeService;
+
+    @PostMapping("/cafePosts/{cafePostId}/members/{membersId}")
+    public ResponseEntity<Void> addLike(
+            @PathVariable Long cafePostId,
+            @PathVariable Long membersId
+    ) {
+        likeService.doLike(cafePostId, membersId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/cafePosts/{cafePostId}")
+    public ResponseEntity<Integer> getLikes(
+            @PathVariable Long cafePostId
+    ) {
+        Integer response = likeService.countLikes(cafePostId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/{likeId}")
+    public ResponseEntity<Void> deleteLike(
+            @PathVariable Long likeId
+    ) {
+        likeService.undoLike(likeId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
