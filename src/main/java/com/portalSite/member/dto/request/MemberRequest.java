bde1ae611e908a907ba49fa9c3441e3bbb873afe
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-04-26 02:45:33 +0900

    ✨Feature : 멤버 CRUD 시작

---
+++ src/main/java/com/portalSite/member/dto/request/MemberRequest.java
@@ -0,0 +1,29 @@
+package com.portalSite.member.dto.request;
+
+import jakarta.validation.constraints.*;
+
+public record MemberRequest(
+        @NotBlank(message = "사용하실 이메일을 입력해주세요.")
+        @Email(message = "이메일 양식에 맞춰 입력해주세요.")
+        String email,
+
+        @Size(min = 10, max = 15, message = "로그인 아이디는 10에서 15자 사이로 입력해주세요.")
+        String loginId,
+
+        @NotBlank(message = "패스워드를 입력해주세요")
+        @Size(min = 12, max = 20, message = "비밀번호는 12에서 20자 사이로 입력해주세요.")
+        String password,
+
+        @NotBlank(message = "이름을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "이름은 2~20글자 사이여야 합니다.")
+        String name,
+
+        @NotBlank(message = "전화번호를 입력해주세요.")
+        @Pattern(regexp = "^010\\d{8}$", message = "전화번호는 010으로 시작하는 11자리 숫자여야 합니다.")
+        String phoneNumber,
+
+        @NotBlank(message = "별명을 입력해주세요.")
+        @Size(min = 4, max = 10, message = "별명은 4에서 10글자 사이의 단어여야 합니다.")
+        String nickname
+) {
+}
---
+++ src/main/java/com/portalSite/member/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.portalSite.member.service;
+
+import com.portalSite.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+}
