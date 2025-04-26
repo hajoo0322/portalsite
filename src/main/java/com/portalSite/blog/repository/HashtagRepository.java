711b45d5d3afd7b413b5fc61588b1af5336b6303
강세민 <semin15151@gmail.com>
2025-04-26 23:30:40 +0900

    🔥Delete : HashtagService.java 삭제

--- src/main/java/com/portalSite/blog/repository/HashtagRepository.java
+++ src/main/java/com/portalSite/blog/repository/HashtagRepository.java
@@ -1,10 +1,12 @@
 package com.portalSite.blog.repository;
 
 import com.portalSite.blog.entity.Hashtag;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag,Long> {
 
+    List<Hashtag> findByTagIn(List<String> tags);
 }
