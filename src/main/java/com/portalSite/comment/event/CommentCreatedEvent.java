7d06617b14b35dad7edb35f17be4b6fca0face05
임PRO스터\su992 <su9926@naver.com>
2025-04-27 02:25:46 +0900

    :sparkles:Feature: 댓글 Event 관련 요소 추가
    - Event 객체, Publisher, Listener 추가

--- src/main/java/com/portalSite/comment/event/CommentCreatedEvent.java
+++ src/main/java/com/portalSite/comment/event/CommentCreatedEvent.java
@@ -2,17 +2,14 @@ package com.portalSite.comment.event;
 
 import com.portalSite.cafe.entity.CafePost;
 import com.portalSite.comment.entity.Comment;
-import com.portalSite.member.entity.Member;
 import lombok.Getter;
 
 @Getter
 public class CommentCreatedEvent {
-    private final Member member;
     private final CafePost post;
     private final Comment comment;
 
-    public CommentCreatedEvent(Member member, CafePost post, Comment comment) {
-        this.member = member;
+    public CommentCreatedEvent(CafePost post, Comment comment) {
         this.post = post;
         this.comment = comment;
     }
