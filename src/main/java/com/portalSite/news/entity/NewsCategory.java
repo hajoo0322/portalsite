e4b7bea062e379545c04d7f4516ee1026df3b029
임PRO스터\su992 <su9926@naver.com>
2025-04-27 02:18:13 +0900

    :sparkles:Feature: NewsCategory Entity 추가

--- src/main/java/com/portalSite/news/entity/NewsCategory.java
+++ src/main/java/com/portalSite/news/entity/NewsCategory.java
@@ -3,13 +3,19 @@ package com.portalSite.news.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,29 +24,37 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NewsCategory {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "news_category_id")
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "news_category_id")
+  private Long id;
 
-	@Column(name = "category_name")
-	private String categoryName;
+  @Column(name = "category_name")
+  private String name;
 
-	@CreatedDate
-	@Column(name = "created_at", nullable = false, updatable = false)
-	private LocalDateTime createdAt;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id")
+  private NewsCategory parent; //상위 카테고리
 
-	private NewsCategory(String categoryName) {
-		this.categoryName = categoryName;
-	}
+  @OneToMany(mappedBy = "parent")
+  private List<NewsCategory> children = new ArrayList<>(); //하위 카테고리
 
-	public static NewsCategory of(String categoryName) {
-		return new NewsCategory(categoryName);
-	}
+  private NewsCategory(String name, NewsCategory parent) {
+    this.name = name;
+    this.parent = parent;
+  }
+
+  public static NewsCategory of(String name, NewsCategory parent) {
+    return new NewsCategory(name, parent);
+  }
+
+  public void updateCategory(String name, NewsCategory parent) {
+    if(parent!=null) this.parent=parent;
+    if(name!=null) this.name=name;
+  }
 }
