068df1260d3fe4661645603aee5393a24ff1c217
lcy <plmokn48@daum.net>
2025-04-27 23:04:27 +0900

    ✨Feature: Secure 어노테이션 활성화

--- src/main/java/com/portalSite/PortalSiteApplication.java
+++ src/main/java/com/portalSite/PortalSiteApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties({JwtSecurityProperties.class})
+@EnableMethodSecurity(securedEnabled = true)
 public class PortalSiteApplication {
 
 	public static void main(String[] args) {
