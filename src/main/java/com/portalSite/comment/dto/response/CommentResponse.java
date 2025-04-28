cb6053d61c745a2be423ab86e1a30a70099ec436
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-04-28 09:24:56 +0900

    🧹Chore : 필드값 순서 변경

--- src/main/java/com/portalSite/comment/dto/response/CommentResponse.java
+++ src/main/java/com/portalSite/comment/dto/response/CommentResponse.java
@@ -6,20 +6,22 @@ import com.portalSite.comment.entity.PostType;
 import java.time.LocalDateTime;
 
 public record CommentResponse(
-        PostType postType,
         Long id,
         Long memberId,
         String memberName,
+        PostType postType,
+        Long postId,
         String content,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
     public static CommentResponse from(Comment comment) {
         return new CommentResponse(
-                comment.getPostType(),
                 comment.getId(),
                 comment.getMember().getId(),
                 comment.getMember().getName(),
+                comment.getPostType(),
+                comment.getPostId(),
                 comment.getContent(),
                 comment.getCreatedAt(),
                 comment.getUpdatedAt()
