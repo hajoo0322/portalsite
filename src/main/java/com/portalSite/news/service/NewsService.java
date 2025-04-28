55eb8f56e6bda16f27188570d1577dba93f8bf59
임PRO스터\su992 <su9926@naver.com>
2025-04-28 16:56:32 +0900

    🧹Chore: 코드 컨벤션 적용 및 불필요한 import문 삭제

--- src/main/java/com/portalSite/news/service/NewsService.java
+++ src/main/java/com/portalSite/news/service/NewsService.java
@@ -8,7 +8,9 @@ import com.portalSite.news.entity.News;
 import com.portalSite.news.entity.NewsCategory;
 import com.portalSite.news.repository.NewsCategoryRepository;
 import com.portalSite.news.repository.NewsRepository;
+
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,66 +19,66 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class NewsService {
 
-  private final NewsRepository newsRepository;
-  private final MemberRepository memberRepository;
-  private final NewsCategoryRepository newsCategoryRepository;
+    private final NewsRepository newsRepository;
+    private final MemberRepository memberRepository;
+    private final NewsCategoryRepository newsCategoryRepository;
 
-  @Transactional
-  public News createNews(NewsCreateRequest requestDto, Long memberId) {
-    Member member = memberRepository.findById(memberId).orElseThrow(
-        () -> new RuntimeException() /*TODO 예외 처리 추가(존재하지 않는 작성자)*/
-    );
-
-    NewsCategory newsCategory = newsCategoryRepository.findById(requestDto.categoryId())
-        .orElseThrow(
-            () -> new RuntimeException() /*TODO 예외 처리 추가(존재하지 않는 카테고리)*/
+    @Transactional
+    public News createNews(NewsCreateRequest requestDto, Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new RuntimeException() /*TODO 예외 처리 추가(존재하지 않는 작성자)*/
         );
 
-    News news = News.of(member, newsCategory, requestDto.newsTitle(), requestDto.description());
+        NewsCategory newsCategory = newsCategoryRepository.findById(requestDto.categoryId())
+                .orElseThrow(
+                        () -> new RuntimeException() /*TODO 예외 처리 추가(존재하지 않는 카테고리)*/
+                );
 
-    return newsRepository.save(news);
-  }
+        News news = News.of(member, newsCategory, requestDto.newsTitle(), requestDto.description());
 
-  @Transactional(readOnly = true)
-  public News getNewsById(Long newsId) {
-    return newsRepository.findById(newsId).orElseThrow(
-        () -> new RuntimeException() /*TODO 예외 처리: (잘못된 리소스, 기사가 존재하지 않음)*/
-    );
-  }
-
-  @Transactional(readOnly = true)
-  public List<News> getNewsListByCategory(Long categoryId) {
-    return newsRepository.findAllByNewsCategoryId(categoryId);
-  }
-
-  @Transactional
-  public News updateNews(NewsRequest requestDto, Long newsId, Long memberId) {
-    Member member = memberRepository.findById(memberId).orElseThrow(
-        () -> new RuntimeException() /*TODO 예외 처리 추가(수정으로 요청한 사용자를 찾을 수 없음)*/
-    );
-
-    News news = getNewsById(newsId);
-    if (!memberId.equals(news.getMember().getId())) {
-      throw new RuntimeException(); /*TODO 예외처리: 뉴스 작성자와 수정 요청자가 다름(수정권한 없음)*/
+        return newsRepository.save(news);
     }
 
-    NewsCategory newsCategory = newsCategoryRepository.findById(requestDto.categoryId())
-        .orElseThrow(
-            () -> new RuntimeException() /*TODO 예외 처리 추가(존재하지 않는 카테고리)*/
+    @Transactional(readOnly = true)
+    public News getNewsById(Long newsId) {
+        return newsRepository.findById(newsId).orElseThrow(
+                () -> new RuntimeException() /*TODO 예외 처리: (잘못된 리소스, 기사가 존재하지 않음)*/
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<News> getNewsListByCategory(Long categoryId) {
+        return newsRepository.findAllByNewsCategoryId(categoryId);
+    }
+
+    @Transactional
+    public News updateNews(NewsRequest requestDto, Long newsId, Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new RuntimeException() /*TODO 예외 처리 추가(수정으로 요청한 사용자를 찾을 수 없음)*/
         );
 
-    news.updateNews(newsCategory, requestDto.newsTitle(), requestDto.description());
+        News news = getNewsById(newsId);
+        if (!memberId.equals(news.getMember().getId())) {
+            throw new RuntimeException(); /*TODO 예외처리: 뉴스 작성자와 수정 요청자가 다름(수정권한 없음)*/
+        }
 
-    return news;
-  }
+        NewsCategory newsCategory = newsCategoryRepository.findById(requestDto.categoryId())
+                .orElseThrow(
+                        () -> new RuntimeException() /*TODO 예외 처리 추가(존재하지 않는 카테고리)*/
+                );
 
-  @Transactional
-  public void deleteNews(Long newsId, Long memberId) {
-    News news = getNewsById(newsId);
-    if (!memberId.equals(news.getMember().getId())) {
-      throw new RuntimeException(); /*TODO 예외처리: 뉴스 작성자와 삭제 요청자가 다름(삭제권한 없음)*/
+        news.updateNews(newsCategory, requestDto.newsTitle(), requestDto.description());
+
+        return news;
     }
 
-    newsRepository.delete(news);
-  }
+    @Transactional
+    public void deleteNews(Long newsId, Long memberId) {
+        News news = getNewsById(newsId);
+        if (!memberId.equals(news.getMember().getId())) {
+            throw new RuntimeException(); /*TODO 예외처리: 뉴스 작성자와 삭제 요청자가 다름(삭제권한 없음)*/
+        }
+
+        newsRepository.delete(news);
+    }
 }
