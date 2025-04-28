200868b3b9b2bc34e1d7de307b080470a22f1fa1
임PRO스터\su992 <su9926@naver.com>
2025-04-28 16:56:45 +0900

    🧹Chore: 코드 컨벤션 적용

--- src/main/java/com/portalSite/news/service/NewsCategoryService.java
+++ src/main/java/com/portalSite/news/service/NewsCategoryService.java
@@ -3,11 +3,12 @@ package com.portalSite.news.service;
 import com.portalSite.news.dto.request.NewsCategoryRequest;
 import com.portalSite.news.dto.response.NewsCategoryListResponse;
 import com.portalSite.news.dto.response.NewsCategoryResponse;
-import com.portalSite.news.entity.News;
 import com.portalSite.news.entity.NewsCategory;
 import com.portalSite.news.repository.NewsCategoryRepository;
 import jakarta.validation.Valid;
+
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,80 +17,80 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class NewsCategoryService {
 
-  private final NewsCategoryRepository newsCategoryRepository;
+    private final NewsCategoryRepository newsCategoryRepository;
 
-  @Transactional
-  public NewsCategoryResponse createCategory(@Valid NewsCategoryRequest request) {
-    if (newsCategoryRepository.findByName(request.name()).isPresent()) {
-      throw new RuntimeException(); /*TODO 예외처리 (이미 존재하는 카테고리)*/
+    @Transactional
+    public NewsCategoryResponse createCategory(@Valid NewsCategoryRequest request) {
+        if (newsCategoryRepository.findByName(request.name()).isPresent()) {
+            throw new RuntimeException(); /*TODO 예외처리 (이미 존재하는 카테고리)*/
+        }
+
+        NewsCategory parentCategory = null;
+        if (null != request.parentId()) {
+            parentCategory = newsCategoryRepository.findById(request.parentId()).orElseThrow(
+                    () -> new RuntimeException() /*TODO 예외처리 (상위 카테고리를 찾을 수 없음)*/
+            );
+        }
+
+        return NewsCategoryResponse.from(newsCategoryRepository.save(NewsCategory.of(request.name(), parentCategory)));
     }
 
-    NewsCategory parentCategory = null;
-    if (null!=request.parentId()) {
-      parentCategory = newsCategoryRepository.findById(request.parentId()).orElseThrow(
-          () -> new RuntimeException() /*TODO 예외처리 (상위 카테고리를 찾을 수 없음)*/
-      );
+    @Transactional(readOnly = true)
+    public NewsCategoryListResponse getSubCategoriesByParentId(Long parentId) {
+        if (!newsCategoryRepository.existsById(parentId)) {
+            throw new RuntimeException(); /*TODO 예외처리(존재하지 않는 카테고리)*/
+        }
+
+        List<NewsCategoryResponse> newsList = newsCategoryRepository.findAllByParentId(parentId).stream()
+                .map(NewsCategoryResponse::from)
+                .toList();
+
+        if (newsList.isEmpty()) {
+            return NewsCategoryListResponse.from("해당 카테고리에 존재하는 뉴스가 없습니다.", null);
+        }
+        return NewsCategoryListResponse.from("뉴스 조회 성공", newsList);
     }
 
-    return NewsCategoryResponse.from(newsCategoryRepository.save(NewsCategory.of(request.name(), parentCategory)));
-  }
+    @Transactional
+    public NewsCategory updateCategory(NewsCategoryRequest request, Long categoryId) {
+        NewsCategory newsCategory = newsCategoryRepository.findById(categoryId).orElseThrow(
+                () -> new RuntimeException() /*TODO 예외처리(존재하지 않는 카테고리)*/
+        );
 
-  @Transactional(readOnly = true)
-  public NewsCategoryListResponse getSubCategoriesByParentId(Long parentId) {
-    if(!newsCategoryRepository.existsById(parentId)) {
-      throw new RuntimeException(); /*TODO 예외처리(존재하지 않는 카테고리)*/
+        NewsCategory parentCategory = null;
+        if (null != request.parentId()) {
+            parentCategory = newsCategoryRepository.findById(request.parentId()).orElseThrow(
+                    () -> new RuntimeException() /*TODO 예외처리(상위 카테고리를 찾을 수 없음)*/
+            );
+        }
+
+        newsCategory.updateCategory(request.name(), parentCategory);
+
+        return newsCategory;
     }
 
-    List<NewsCategoryResponse> newsList = newsCategoryRepository.findAllByParentId(parentId).stream()
-            .map(NewsCategoryResponse::from)
-            .toList();
+    @Transactional
+    public void deleteCategory(Long categoryId) {
+        NewsCategory newsCategory = newsCategoryRepository.findById(categoryId).orElseThrow(
+                () -> new RuntimeException() /*TODO 예외처리(존재하지 않는 카테고리)*/
+        );
 
-    if (newsList.isEmpty()){
-     return NewsCategoryListResponse.from("해당 카테고리에 존재하는 뉴스가 없습니다.", null);
-    }
-    return NewsCategoryListResponse.from("뉴스 조회 성공", newsList);
-  }
+        deleteSubcategories(categoryId);
 
-  @Transactional
-  public NewsCategory updateCategory(NewsCategoryRequest request, Long categoryId) {
-    NewsCategory newsCategory = newsCategoryRepository.findById(categoryId).orElseThrow(
-        () -> new RuntimeException() /*TODO 예외처리(존재하지 않는 카테고리)*/
-    );
-
-    NewsCategory parentCategory = null;
-    if(null!=request.parentId()) {
-      parentCategory = newsCategoryRepository.findById(request.parentId()).orElseThrow(
-          () -> new RuntimeException() /*TODO 예외처리(상위 카테고리를 찾을 수 없음)*/
-      );
+        newsCategoryRepository.delete(newsCategory); //최상위 카테고리 삭제
     }
 
-    newsCategory.updateCategory(request.name(), parentCategory);
+    @Transactional
+    public void deleteSubcategories(Long parentId) {
+        List<NewsCategory> subCategoryList = newsCategoryRepository.findAllByParentId(parentId);
 
-    return newsCategory;
-  }
+        if (subCategoryList.isEmpty()) {
+            return;
+        }
 
-  @Transactional
-  public void deleteCategory(Long categoryId) {
-    NewsCategory newsCategory = newsCategoryRepository.findById(categoryId).orElseThrow(
-        () -> new RuntimeException() /*TODO 예외처리(존재하지 않는 카테고리)*/
-    );
-
-    deleteSubcategories(categoryId);
-
-    newsCategoryRepository.delete(newsCategory); //최상위 카테고리 삭제
-  }
-
-  @Transactional
-  public void deleteSubcategories(Long parentId) {
-    List<NewsCategory> subCategoryList = newsCategoryRepository.findAllByParentId(parentId);
-
-    if(subCategoryList.isEmpty()){
-      return;
+        for (NewsCategory newsCategory : subCategoryList) {
+            deleteSubcategories(newsCategory.getId());
+            newsCategoryRepository.delete(newsCategory); //관련 하위 카테고리 모두 삭제
+        }
     }
-
-    for (NewsCategory newsCategory : subCategoryList){
-      deleteSubcategories(newsCategory.getId());
-      newsCategoryRepository.delete(newsCategory); //관련 하위 카테고리 모두 삭제
-    }
-  }
 }
