4761a1ef0e455705e14e7a316203a5b77b9e0a35
lcy <plmokn48@daum.net>
2025-04-29 17:36:05 +0900

    ✨Feature: rawAnswer가 null일 경우 분기처리

--- src/main/java/com/portalSite/chatbot/repository/MessageBroadcaster.java
+++ src/main/java/com/portalSite/chatbot/repository/MessageBroadcaster.java
@@ -3,5 +3,6 @@ package com.portalSite.chatbot.repository;
 import com.portalSite.chatbot.dto.ChatbotAnswerMessage;
 
 public interface MessageBroadcaster {
+
     void send(ChatbotAnswerMessage message);
 }
--- src/main/java/com/portalSite/chatbot/repository/MessageBroker.java
+++ src/main/java/com/portalSite/chatbot/repository/MessageBroker.java
@@ -3,5 +3,6 @@ package com.portalSite.chatbot.repository;
 import com.portalSite.chatbot.dto.ChatbotAnswerMessage;
 
 public interface MessageBroker {
+
     void publish(ChatbotAnswerMessage message);
 }
