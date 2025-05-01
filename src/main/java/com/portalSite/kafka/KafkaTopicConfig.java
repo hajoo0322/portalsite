d2d3e9b1080e476688dfb1fab9ca2ef38fda58d7
강세민 <semin15151@gmail.com>
2025-05-01 14:14:27 +0900

    ✨Feature : 실시간 검색어 랭킹 집계 기능

--- src/main/java/com/portalSite/kafka/KafkaTopicConfig.java
+++ src/main/java/com/portalSite/kafka/KafkaTopicConfig.java
@@ -1,7 +1,11 @@
-package com.portalSite.common.config;
+package com.portalSite.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
+@Configuration
 public class KafkaTopicConfig {
 
     @Bean
@@ -11,6 +15,4 @@ public class KafkaTopicConfig {
             .replicas(1)
             .build();
     }
-
-
 }
