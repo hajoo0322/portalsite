973d8a2b03365acd9196b07135088ca74e732aeb
임PRO스터\su992 <su9926@naver.com>
2025-05-02 16:04:59 +0900

    :sparkles:Feature: top keywords 반환 API 구현

--- src/main/java/com/portalSite/search/controller/SearchController.java
+++ src/main/java/com/portalSite/search/controller/SearchController.java
@@ -1,16 +1,21 @@
 package com.portalSite.search.controller;
 
-import com.portalSite.search.dto.request.SearchRequest;
+import com.portalSite.kafka.KeywordProducer;
 import com.portalSite.search.dto.response.SearchResponse;
+import com.portalSite.search.dto.response.TopKeywordsResponse;
 import com.portalSite.search.service.SearchService;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
@@ -19,6 +24,8 @@ import static org.springframework.data.domain.Sort.Direction.DESC;
 @RequiredArgsConstructor
 public class SearchController {
     private final SearchService searchService;
+    private final KeywordProducer keywordProducer;
+    private final RedisTemplate<String,String> redisTemplate;
 
     @GetMapping
     public ResponseEntity<SearchResponse> search(
@@ -27,7 +34,27 @@ public class SearchController {
             @Size(min = 1, message = "검색어는 1글자 이상 입력해주세요")String keyword,
             @PageableDefault(sort = "id", direction = DESC) Pageable pageable
             ) {
+        keywordProducer.publishRawKeyword(keyword); //kafka로 검색어 publish
         SearchResponse response = searchService.search(keyword, pageable);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/top-keywords")
+    public ResponseEntity<List<TopKeywordsResponse>> getKeywordRanking(){
+        //아래 linkedHashSet은 삽입 순서 유지
+        Set<String> result = redisTemplate.opsForZSet().reverseRange("popular:keywords", 0, 9);
+
+        if(result == null || result.isEmpty()) {
+            throw new RuntimeException(); /*TODO notfound 예외처리*/
+        }
+
+        List<TopKeywordsResponse> responseList = new ArrayList<>();
+
+        int rank = 1;
+        for(String keyword : result) {
+            responseList.add(new TopKeywordsResponse(rank++, keyword));
+        }
+
+        return ResponseEntity.ok(responseList);
     }
 }
