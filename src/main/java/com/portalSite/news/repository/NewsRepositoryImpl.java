43ef417e07219aa8495f725d92841b22dc36a542
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-05-09 03:57:13 +0900

    🔥Delete : QueryDSL 관련 로직, 임포트 삭제(News)

--- src/main/java/com/portalSite/news/repository/NewsRepositoryImpl.java
+++ src/main/java/com/portalSite/news/repository/NewsRepositoryImpl.java
@@ -1,11 +1,6 @@
 package com.portalSite.news.repository;
 
 import com.portalSite.news.dto.response.NewsResponse;
-import com.portalSite.news.entity.News;
-import com.portalSite.news.entity.QNews;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
@@ -19,56 +14,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 public class NewsRepositoryImpl implements NewsRepositoryCustom{
-    private final JPAQueryFactory queryFactory;
-
     private final EntityManager entityManager;
-    @Override
-    public Page<News> findAllByKeyword(
-            String keyword, String writer,
-            LocalDateTime createdAtStart, LocalDateTime createdAtEnd,
-            boolean descending, Pageable pageable) {
-
-        QNews news = QNews.news;
-
-        BooleanBuilder builder = new BooleanBuilder();
-
-        // 기본 검색 조건(타이틀, 내용)
-        builder.and(news.newsTitle.containsIgnoreCase(keyword))
-                .or(news.description.containsIgnoreCase(keyword));
-
-        // 작성자 필터
-        if (createdAtStart != null) {
-            builder.and(news.member.name.containsIgnoreCase(writer));
-        }
-
-        // 날짜 필터
-        if (createdAtStart != null) {
-            builder.and(news.createdAt.goe(createdAtStart));
-        }
-        if (createdAtEnd != null) {
-            builder.and(news.createdAt.loe(createdAtEnd));
-        }
-
-        // 정렬 조건 설정
-        OrderSpecifier<?> order = descending ?
-                news.createdAt.desc() : news.createdAt.asc();
-
-        List<News> result = queryFactory
-                .selectFrom(news).distinct()
-                .where(builder)
-                .orderBy(order)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-
-        Long total = queryFactory
-                .select(news.countDistinct())
-                .from(news)
-                .where(builder)
-                .fetchOne();
-
-        return new PageImpl<>(result, pageable, total != null ? total : 0);
-    }
 
     @Override
     public Page<NewsResponse> findAllByKeywordV2(
