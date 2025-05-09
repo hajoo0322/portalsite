56169c2fc78551bca088fa8a2ec388ea1ea510cd
임PRO스터\su992 <su9926@naver.com>
2025-05-09 09:57:03 +0900

    :sparkles:Feature: Pageable이 추가된 뉴스 카테고리 리스트 조회 메서드 구현

--- src/main/java/com/portalSite/news/repository/NewsCategoryRepository.java
+++ src/main/java/com/portalSite/news/repository/NewsCategoryRepository.java
@@ -1,14 +1,17 @@
 package com.portalSite.news.repository;
 
 import com.portalSite.news.entity.NewsCategory;
-import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewsCategoryRepository extends JpaRepository<NewsCategory, Long> {
 
   Optional<NewsCategory> findByName(String name);
+
+  List<NewsCategory> findByParentId(Long categoryId, Pageable pageable);
 
   List<NewsCategory> findAllByParentId(Long categoryId);
 }
