e6d0243e359bbb9d88d81985fa9f2db5e794d6a9
임PRO스터\su992 <su9926@naver.com>
2025-05-21 11:20:30 +0900

    :sparkles:Feature: 엘라스틱서치 적용 검색 API 구현

--- src/main/java/com/portalSite/search/controller/SearchController.java
+++ src/main/java/com/portalSite/search/controller/SearchController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -56,6 +57,19 @@ public class SearchController {
         keywordProducer.publishRawKeywordInputEvent(keyword); //kafka로 검색어 publish
         SearchResponse response = searchService.searchV2(keyword, pageable, postType);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/es")
+    public ResponseEntity<SearchResponse> searchFromES(
+            @RequestParam("keyword")
+            @NotBlank(message = "검색어를 입력해주세요")
+            @Size(min = 1, message = "검색어는 1글자 이상 입력해주세요") String keyword,
+            @RequestParam(value = "postType", required = false) PostType postType,
+            @PageableDefault(sort = "id", direction = DESC) Pageable pageable
+    ) {
+        keywordProducer.publishRawKeywordInputEvent(keyword);
+        SearchResponse response = searchService.searchByElasticsearch(keyword, pageable, postType);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/top-keywords")
--- src/main/java/com/portalSite/search/service/SearchService.java
+++ src/main/java/com/portalSite/search/service/SearchService.java
@@ -1,5 +1,8 @@
 package com.portalSite.search.service;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import com.portalSite.blog.dto.response.BlogPostResponse;
 import com.portalSite.blog.repository.BlogPostRepository;
 import com.portalSite.cafe.dto.CafePostResponse;
@@ -7,12 +10,13 @@ import com.portalSite.cafe.repository.CafePostRepository;
 import com.portalSite.comment.entity.PostType;
 import com.portalSite.news.dto.response.NewsResponse;
 import com.portalSite.news.repository.NewsRepository;
-import com.portalSite.search.dto.request.SearchRequest;
+
 import com.portalSite.search.dto.response.SearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -21,6 +25,7 @@ public class SearchService {
     private final BlogPostRepository blogPostRepository;
     private final CafePostRepository cafePostRepository;
     private final NewsRepository newsRepository;
+    private final ElasticsearchClient esClient;
 
     public SearchResponse search(String keyword, Pageable pageable, PostType postType) {
 
@@ -52,4 +57,43 @@ public class SearchService {
 
         return SearchResponse.from(blogPostList, cafePostList, newsList, pageable);
     }
+
+    public SearchResponse searchByElasticsearch(String keyword, Pageable pageable, PostType postType) {
+        List<BlogPostResponse> blogPostList = (postType == null || postType == PostType.BLOG) ?
+                searchGeneric("blog_post_index", keyword, List.of("title", "description"), pageable, BlogPostResponse.class, "id") : null;
+
+        List<CafePostResponse> cafePostList = (postType == null || postType == PostType.CAFE) ?
+                searchGeneric("cafe_post_index", keyword, List.of("title", "description"), pageable, CafePostResponse.class, "id") : null;
+
+        List<NewsResponse> newsList = (postType == null || postType == PostType.NEWS) ?
+                searchGeneric("news_index", keyword, List.of("newsTitle", "description"), pageable, NewsResponse.class, "newsId") : null;
+
+        return SearchResponse.from(blogPostList, cafePostList, newsList, pageable);
+    }
+
+    private <T> List<T> searchGeneric(String indexName, String keyword, List<String> fields, Pageable pageable, Class<T> responseClass, String sortField) {
+        try{
+            //ES 검색 및 결과는 Class<T>로 바로 매핑
+            var response = esClient.search(s->s
+                            .index(indexName)  //검색할 인덱스 지정
+                            .query(q->q  //검색 조건 설정.  multi_match 쿼리로 여러 필드에 대해 검색
+                                    .multiMatch(m->m
+                                            .fields(fields)
+                                            .query(keyword)
+                                    )
+                            )
+                            .from((int) pageable.getOffset())  //페이징 처리
+                            .size(pageable.getPageSize())
+                            .sort(srt -> srt
+                                    .field(f->f.field(sortField).order(SortOrder.Desc))  //id를 기준으로 내림차순 정렬
+                            ),
+                    responseClass //결과 매핑
+            );
+
+            return response.hits().hits().stream().map(Hit::source).toList();  //ES 에서 주요내용(Class<T>)만 리스트로 반환
+        }catch (IOException e){
+            throw new RuntimeException("ES 검색 실패. indexName = "+indexName, e);
+        }
+    }
+
 }
